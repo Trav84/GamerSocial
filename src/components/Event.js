da882a73b2c38daab104c6224a44985861274f41
travisczerw <travis.czerw@gmail.com>
2016-03-22 23:01:01 -0500

    refactor(event): basic form component work

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,28 +1,77 @@
-/**
- * Created by trav84 on 2/21/16.
- */
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 class Event extends React.Component {
-  componentDidMount = () => {
-    $('.datepicker').pickadate();
+  static defaultProps = {
   };
+
+  static propTypes = {
+  };
+
+  state = {
+    game: '',
+    eventDate: ''
+  };
+
+  componentDidMount = () => {
+    //Init datapicker.js, part of Materalize
+    //$('.datepicker').pickadate({
+    //  formatSubmit: 'yyyy/mm/dd'
+    //});
+  };
+
+  handleGameChange = (e) => {
+    this.setState({game: e.target.value});
+  };
+
+  handleEventDateChange = (e) => {
+      this.setState({eventDate: e.target.value});
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted');
+    var game = this.state.game;
+    var date = this.state.eventDate;
+    if(!game || !date) {
+      return;
+    }
+
+    this.setState({
+      game: '',
+      eventDate: ''
+    })
+  };
+
   render() {
     return (
       <section className="event">
         <h1>Add New Gaming Event</h1>
         <div className="row">
-          <form className="col s12">
+          <form onSubmit={ this.onSubmit } className="col s12">
             <div className="row">
               <div className="input-field col s12">
-                <input placeholder="Minecraft" id="game" type="text" className="validate"/>
-                  <label htmlFor="game">What Game?</label>
+                <input
+                  onChange={ this.handleGameChange }
+                  value={ this.state.game }
+                  placeholder="Minecraft"
+                  id="game"
+                  type="text"
+                  className="validate"
+                />
+                <label htmlFor="game">What Game?</label>
               </div>
               <div className="input-field col s12">
-                <input id="date" type="date" className="validate datepicker"/>
-                  <label htmlFor="date">When?</label>
+                <input
+                  value={ this.state.eventDate }
+                  onChange={ this.handleEventDateChange }
+                  id="date"
+                  type="date"
+                />
               </div>
+              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                <i className="material-icons right">send</i>
+              </button>
             </div>
           </form>
         </div>
