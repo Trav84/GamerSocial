3486e9f113c40e1b1d26a590b79183a61f8c7835
travisczerw <travis.czerw@gmail.com>
2016-05-23 20:10:24 -0500

    refactor(header): add in links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,5 @@
-/**
- * Created by trav84 on 2/21/16.
- */
 import React from 'react';
-import { Link, history } from 'react-router'
+import { Link } from 'react-router';
 
 class Header extends React.Component {
   render() {
@@ -13,20 +10,21 @@ class Header extends React.Component {
           <a href="/src" className="brand-logo">Logo</a>
           <a href="#" data-activates="nav-mobile" className="button-collapse"><i className="material-icons">menu</i></a>
           <ul id="nav" className="right hide-on-med-and-down">
-            <li><Link to="createevent">Create Game Event</Link></li>
-            <li><Link to="eventlist">View Games</Link></li>
-            <li><a href="#">Your Queue</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Sign Out</a></li>
+            <li><Link to="/create">Create Event</Link></li>
+            <li><Link to="/games">View Games</Link></li>
+            <li><Link to="/queue">Your Queue</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/login">Sign In</Link></li>
           </ul>
           <ul id="nav-mobile" className="side-nav">
-            <li><a href="#">Create Game Event</a></li>
-            <li><a href="#">View Games</a></li>
-            <li><a href="#">Your Queue</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Sign Out</a></li>
+            <li><Link to="/create">Create Event</Link></li>
+            <li><Link to="/games">View Games</Link></li>
+            <li><Link to="/queue">Your Queue</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/login">Sign In</Link></li>
           </ul>
         </div>
+        {this.props.children}
       </nav>
         {this.props.children}
       </div>
