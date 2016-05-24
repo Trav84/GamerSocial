7236a3fe9b1979ba56886f534eb49336dd7d722c
travisczerw <travis.czerw@gmail.com>
2016-05-23 20:10:58 -0500

    feat(login & regsiter): add components for login and register

---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://gamerhub.firebaseio.com/');
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  login = (service) => {
+    base.authWithOAuthPopup(service, function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+  };
+
+  render() {
+    return (
+      <section className="login">
+        <div className="row">
+          <a className="waves-effect waves-light btn" onClick={ () => { this.login("google") } }>Google</a>
+        </div>
+        <div className="row">
+          <a className="waves-effect waves-light btn" onClick={ () => { this.login("facebook") } }>Facebook</a>
+        </div>
+        <div className="row">
+          <a className="waves-effect waves-light btn">Register</a>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Login;
---
+++ src/components/Register.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://gamerhub.firebaseio.com/');
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  };
+
+  login = () => {
+
+  };
+
+  render() {
+    return (
+      <section className="register">
+
+      </section>
+    );
+  }
+}
+
+export default Login;
