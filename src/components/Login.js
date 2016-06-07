c9e35b7af638ffff9a68ea28575135b5634b7d7b
travisczerw <travis.czerw@gmail.com>
2016-06-06 19:48:52 -0500

    refactor(login & auth): users can create via simple email/pass and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link } from 'react-router';
+import { Link, Router } from 'react-router';
 
 var Rebase = require('re-base');
 var base = Rebase.createClass('https://gamerhub.firebaseio.com/');
@@ -10,6 +10,18 @@ import "./login.scss";
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  };
+
+  handleEmailChange = (e) => {
+    this.setState({email: e.target.value});
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value});
   };
 
   login (service) {
@@ -18,8 +30,36 @@ class Login extends React.Component {
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
+        // Create
+        base.createUser({
+          name: authData.displayName,
+          password: 'test',
+          email: 'tet@test.com'
+        });
       }
     });
+  };
+
+  simpleLogin = () => {
+    // Simple email authentication
+    base.authWithPassword({
+      email    : this.state.email,
+      password : this.state.password
+    }, function(err) {
+      console.log(err);
+    });
+
+    //// Listen to authentication
+    function authDataCallback(authData) {
+      if (authData) {
+        console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      }
+      else {
+        console.log("User is logged out");
+      }
+    }
+
+    base.onAuth(this.authDataCallback);
   };
 
   render() {
@@ -30,6 +70,31 @@ class Login extends React.Component {
         </div>
         <div className="row">
           <a className="waves-effect waves-light btn" onClick={ () => { this.login("facebook") } }>Facebook</a>
+        </div>
+        <div className="row">
+        <div className="input-field col s12">
+          <input
+            placeholder="Bobsmith@gmail.com"
+            id="email"
+            type="text"
+            className="validate"
+            onChange={this.handleEmailChange}
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div className="input-field col s12">
+          <input
+            placeholder="*****"
+            id="password"
+            type="password"
+            className="validate"
+            onChange={this.handlePasswordChange}
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+        </div>
+        <div className="row">
+          <a className="waves-effect waves-light btn" onClick={ () => { this.simpleLogin() } }>Login</a>
         </div>
         <div className="row">
           <Link to="/Register" className="waves-effect waves-light btn">Register</Link>
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,44 +7,90 @@ var base = Rebase.createClass('https://gamerhub.firebaseio.com/');
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
   };
 
-  login(){
+  handleNameChange = (e) => {
+    this.setState({name: e.target.value});
+  };
 
+  handleEmailChange = (e) => {
+    this.setState({email: e.target.value});
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({password: e.target.value});
+  };
+
+  handleSubmit = () => {
+    // Create a user
+    base.createUser({
+      name: this.state.name,
+      password: this.state.password,
+      email: this.state.email
+    },  function(error, userData) {
+      if (error) {
+        switch (error.code) {
+          case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+          case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+          default:
+            console.log("Error creating user:", error);
+        }
+      }
+      else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
   };
 
   render() {
     return (
       <section className="register">
         <div className="row">
-          <form className="col s12">
+          <form className="col s12" onSubmit={this.handleSubmit}>
             <div className="row">
               <div className="input-field col s6">
-                <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
-                  <label htmlFor="first_name">First Name</label>
-              </div>
-              <div className="input-field col s6">
-                <input id="last_name" type="text" className="validate" />
-                  <label htmlFor="last_name">Last Name</label>
+                <input
+                  placeholder="Bob Smith"
+                  id="name"
+                  type="text"
+                  className="validate"
+                  onChange={this.handleNameChange}
+                />
+                  <label htmlFor="name">Name</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input disabled value="I am not editable" id="disabled" type="text" className="validate" />
-                  <label htmlFor="disabled">Disabled</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="password" type="password" className="validate" />
+                <input
+                  id="password"
+                  type="password"
+                  className="validate"
+                  onChange={this.handlePasswordChange}
+                />
                   <label htmlFor="password">Password</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input id="email" type="email" className="validate" />
+                <input
+                  placeholder="bobsmith@gmail.com"
+                  id="email"
+                  type="email"
+                  className="validate"
+                  onChange={this.handleEmailChange}
+                />
                   <label htmlFor="email">Email</label>
               </div>
+              <input type="submit" value="Submit" />
             </div>
           </form>
         </div>
